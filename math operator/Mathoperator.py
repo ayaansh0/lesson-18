fdebd3d34ccb97487381b02ffe1180d844ec6e63
ayaansh0 <dinoayaaun@gmail.com>
2025-03-18 16:29:23 +0530

    commit

---
+++ math operator/Mathoperator.py
@@ -0,0 +1,17 @@
+import math #importing math module 
+#using ceil and floor function of math module 
+print("the floor and ceiling value of 23.56 are " '+ str (math.ceil(23.56)) + ' ',' + str(math.floor(23.56)))
+
+
+x = 10
+y = -15 
+
+#using copysign fguction 
+print('the value of x after copying the sign from y is : ' + str(math.copysign(x,y)))
+
+
+#using fabs and gcd func
+print('absolute value of -96 and 56 are : '+ str(math.fabs(-96)) + ',' + str(math.fabs(56))) 
+
+
+print('the GCD of 24 and 56 : ' + str (math.gcd(24,56)))
